bcd7f4e37349b4b3d28b4599356ef4ce0513fb70
Wes Gunning <wesgunning@gmail.com>
2022-12-12 17:51:10 -0500

    Add functions to box size button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const container = document.querySelector('#container');
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
-const n = 16;
+let n = 16;
 for (i = 0; i < n; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -13,10 +13,40 @@ for (i = 0; i < n; i++) {
         const div = document.createElement('div');
         div.classList.add('boxes');
         row.appendChild(div);
+        // Listen for mouse hover over individual boxes
         div.addEventListener("mouseover", function (e) {
             e.target.style.background = 'blue';
         });
     }
 }
 
-// Listen for mouse hover over individual boxes
+// Box size button
+const btn = document.querySelector('button');
+btn.addEventListener("click", boxSize);
+
+function removeGrid() {
+    grid.innerHTML = '';
+};
+
+function rebuildGrid(n) {
+    for (i = 0; i < n; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+        for (j = 0; j < n; j++) {
+            const div = document.createElement('div');
+            div.classList.add('boxes');
+            row.appendChild(div);
+            // Listen for mouse hover over individual boxes
+            div.addEventListener("mouseover", function (e) {
+                e.target.style.background = 'blue';
+            });
+        }
+    }
+}
+
+function boxSize() {
+   let n = prompt("What size square would you like?");
+   removeGrid();
+   rebuildGrid(n);
+};
