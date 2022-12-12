87b4828d87e6591a56197c41daedd4e2e3b0d8ea
Wes Gunning <wesgunning@gmail.com>
2022-12-12 15:06:27 -0500

    Modify square to 16x16. Add 'grid' div within 'container' div to center boxes within page

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 // Create 16x16 grid of squares
 
 const container = document.querySelector('#container');
-for (i = 0; i < 4; i++) {
-    const div = document.createElement('div');
-    div.classList.add('boxes');
-    container.appendChild(div);
+const grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
+const n = 16;
+for (i = 0; i < n; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    grid.appendChild(row);
+    for (j = 0; j < n; j++) {
+        const div = document.createElement('div');
+        div.classList.add('boxes');
+        row.appendChild(div);
+    }
 }
